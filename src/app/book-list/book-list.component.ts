5fe758c8c3fad640cb5c505b84b66962258c3764
ErmisV <ermisvasilaros@gmail.com>
2023-03-11 01:27:49 +0200

    a commit

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit} from '@angular/core';
+import {Book} from '../book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit{
+
+  books: Book[];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
+
+}
---
+++ src/app/book.ts
@@ -0,0 +1,7 @@
+export class Book {
+    id: number;
+    bookName: string;
+    bookAuthor: string;
+    bookTag: string;
+    bookPages: number;
+}
